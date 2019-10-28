d2f3514547f1dc48071cdccfd67dee8872634b1c
tanmaysharma1 <45612331+tanmaysharma1@users.noreply.github.com>
2019-10-27 22:02:40 -0500

    Update UTPod.cpp

--- UTPod.cpp
+++ UTPod.cpp
@@ -3,11 +3,13 @@
 //
 
 #include "UTPod.h"
-UTPod::UTPod{
+#include <stdlib.h> 
+UtPod::UtPod(){
+    numSongs=0;
     songs->next =NULL;
     memSize=MAX_MEMORY;
 }
-UTPod::UtPod(int size){
+UtPod::UtPod(int size){
     
     songs->next =NULL;
     if(size>MAX_MEMORY || size <1)
@@ -18,24 +20,56 @@ UTPod::UtPod(int size){
     memSize == size;
     }
 }
-int UTPod:: addSong(Song const &s) {
+int UtPod:: addSong(Song const &s) {
     SongNode *temp = new SongNode;
     temp->next = songs;
     temp->s = s;
     songs = temp;
+    numSongs++;
     return SUCCESS;
     
+    
 }
-int UTPod:: removeSong(Song const &s) {
+int UtPod:: removeSong(Song const &s) {
+    numSongs--;
 }
-void UTPod:: shuffle(){
+void UtPod:: swap(Song const &s1, Song const &s2)
+{
+    Song temp = new Song;
+    temp.setArtist(s2.getArtist());
+    temp.setTitle(s2.getTitle());
+    temp.setTitle(s2.getSize());
+    s2.setArtist(s1.getArtist());
+    s2.setTitle(s1.getTitle());
+    s2.setTitle(s1.getSize());
+    s1.setArtist(temp.getArtist());
+    s1.setTitle(temp.getTitle());
+    s1.setTitle(temp.getSize());
 }
-void UTPod:: showSongList(){
+
+void UtPod:: shuffle(){
+    for(int x=0; x<numSongs;x++){
+       int songIndex = rand() % numSongs;
+        SongNode *ptr = songs;
+        while (songIndex > 0)    {            // keeps going next on the pointer until num is 0
+        ptr = ptr -> next;
+        songIndex --;
+    }
+        SongNode *ptr2 = songs;
+        songIndex=x;
+        while (songIndex > 0)    {            // keeps going next on the pointer until num is 0, and returns the pointer
+        ptr2 = ptr2 -> next;
+        num --;
+    }
+        swap(ptr->s,ptr2->s);
+    }
 }
-void UTPod:: clearMemory(){
+void UtPod:: showSongList(){
 }
-int UTPod:: getTotalMemory(){
+void UtPod:: clearMemory(){
+}
+int UtPod:: getTotalMemory(){
         return memSize;
 }
-int UTPod:: getRemainingMemory(){
+int UtPod:: getRemainingMemory(){
 }

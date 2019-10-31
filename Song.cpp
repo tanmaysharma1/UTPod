fcfec4fcba9cdf6ec582026b6d24e3e6cb548f9c
tanmaysharma1 <45612331+tanmaysharma1@users.noreply.github.com>
2019-10-31 18:53:59 -0500

    Update Song.cpp

--- Song.cpp
+++ Song.cpp
@@ -3,6 +3,65 @@
 //
 #include "Song.h"
 #include <stdlib.h> 
-Song::Song(){
+Song::Song(int size1, string artist1, string title1){
+  size=size1;
+  artist=artist1;
+  title=title1;
+}
+void Song::Song setName(string name){
+  artist=name1;
   
 }
+string Song::Song getName(){
+  return artist;  
+}
+void Song::Song setTitle(string title1){
+  title=title1;
+  
+}
+string Song::Song getTitle(){
+  return title;  
+}
+void Song:Song setSize(int size1){
+  size=size1;
+}
+int Song:Song getSize(){
+  return size;
+}
+bool operator >(Song const &rhs){
+  if(artist < rhs.artist){
+    return true;
+  }
+   if(title < rhs.title){
+    return true;
+  }
+   if(size < rhs.size){
+    return true;
+  }
+  return false;
+}
+bool operator <(Song const &rhs){
+  if(artist > rhs.artist){
+    return true;
+  }
+   if(title > rhs.title){
+    return true;
+  }
+   if(size > rhs.size){
+    return true;
+  }
+  return false;
+}
+bool operator ==(Song const &rhs){
+  if(artist == rhs.artist){
+    return true;
+  }
+   if(title == rhs.title){
+    return true;
+  }
+   if(size == rhs.size){
+    return true;
+  }
+  return false;
+}
+
